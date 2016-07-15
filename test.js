c212bd7c77e3a1a53c32be9263b3ebc81dd32aad
Robin Millette <robin@millette.info>
2016-07-15 15:48:20 -0400

    Unit tests.

--- test.js
+++ test.js
@@ -3,6 +3,14 @@
 import test from 'ava'
 import fn from './'
 
-test('title', t => {
-  t.is(fn('unicorns'), 'unicorns & rainbows')
+test('large', async t => {
+  const result = await fn('FreeCodeCamp/FreeCodeCamp')
+  t.truthy(result.length > 300)
 })
+
+test('small', async t => {
+  const result = await fn('millette/committed-streaker')
+  t.truthy(result.length < 100)
+})
+
+test('not found', async t => await t.throws(fn('millette/committed-streaker666'), 'Response code 404 (Not Found)'))
