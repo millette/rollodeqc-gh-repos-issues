bd5ac4e5a39f71e2c8020d0e0b6fef9cde85e494
Robin Millette <robin@millette.info>
2017-12-05 14:07:44 -0500

    Fix tests.

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ import fn from './'
 
 test('large', async t => {
   const result = await fn('FreeCodeCamp/FreeCodeCamp')
-  t.truthy(result.length > 200 && result.length < 500)
+  t.truthy(result.length > 100 && result.length < 500)
 })
 
 test('small', async t => {
