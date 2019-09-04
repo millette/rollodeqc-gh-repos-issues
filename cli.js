affa6663992c5e9ad5a25c932f1c172ecdf573ad
Robin Millette <robin@millette.info>
2019-09-04 11:12:16 -0400

    Link to proper license file.

--- cli.js
+++ cli.js
@@ -18,7 +18,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
 
 You should have received a copy of the
-[GNU Affero General Public License](LICENSE.md)
+[GNU Affero General Public License](LICENSE.txt)
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
